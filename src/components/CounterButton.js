43146ebae2b16adbc213f8008acc933923503517
Maria Fernanda Chavez <mf.chavezminjarez@gmail.com>
2022-10-18 19:12:16 -0700

    import useState in all components

--- src/components/CounterButton.js
+++ src/components/CounterButton.js
@@ -1,7 +1,11 @@
+import { useState } from "react";
+
 function CounterButton(){
+  const [clickAmount, setClickAmount] = useState(0);
+
   return (
     <button className="CounterButton">
-      You clicked me X amount of times
+      You clicked me {clickAmount} amount of times
     </button>
   );
 }
