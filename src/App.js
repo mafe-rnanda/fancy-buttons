28e80d6a8bc0dfeec861681666c4d98c3bbf995d
Maria Fernanda Chavez <mf.chavezminjarez@gmail.com>
2022-10-19 12:31:35 -0700

    refactor state in the lightbutton

--- src/App.js
+++ src/App.js
@@ -8,14 +8,14 @@ import TextRepeaterButton from './components/TextRepeaterButton';
 function App() {
   const [light, setLight] = useState("off");
   const dark = (light === 'off') ? 'dark' : '';
-  
+  const switchLight = () => setLight(light === "on" ? "off" : "on");
   return (
     <div className={`App ${dark}`}>
       <h1>Fancy Buttons!</h1>
       <section>
         <AngryButton />
         <CounterButton />
-        <LightSwitchButton light={light} setLight={setLight}/>
+        <LightSwitchButton light={light} setLight={setLight} switchLight={switchLight}/>
         <TextRepeaterButton />
       </section>
     </div>
--- src/components/LightSwitchButton.js
+++ src/components/LightSwitchButton.js
@@ -1,12 +1,10 @@
-import { useState } from "react";
-
 function LightSwitchButton(props){
-  const handleClick = () => props.setLight(props.light === "on" ? "off" : "on");
-
+  const { light, switchLight } = props;
+  
   return (
-    <button className="LightSwitchButton" onClick={handleClick}>
-    {props.light === "on" && <span><i>💡</i> I'm on!</span>}
-    {props.light === "off" && <span className="off"><i>💡</i> I'm off!</span>}
+    <button className="LightSwitchButton" onClick={switchLight}>
+    {light === "on" && <span><i>💡</i> I'm on!</span>}
+    {light === "off" && <span className="off"><i>💡</i> I'm off!</span>}
   </button>
   );
 }
